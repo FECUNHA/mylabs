0858f4d19f0ccafd443771803f39dd766cf09f89
fernandoCDev <fecunhainfo@gmail.com>
2016-05-10 00:31:28 -0300

    add print

--- ExceptionsValidationData/index.php
+++ ExceptionsValidationData/index.php
@@ -18,17 +18,26 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title>Use exception for data validation</title>
     </head>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     <body>
         <?php
         
          if( !isset($_POST['submit']) ):
         ?>
         <form action="index.php" method="post">
+            <h1>Login de acesso</h1>
              <div class="form-group">
-                    <input id="loginEmail" name="email" type="text" class="form-control" placeholder="Email" tabindex="1">
+                 <input id="loginEmail" name="email" type="text" class="form-control" placeholder="Email" tabindex="1" style="width:40%">
                 </div>
                 <div class="form-group">
-                    <input id="pwd" name="pwd" type="password" class="form-control" placeholder="password" tabindex="2">
+                    <input id="pwd" name="pwd" type="password" class="form-control" placeholder="password" tabindex="2" style="width:40%">
                  </div>
                 <div class="form-group">
                     <input class="btn btn-primary" name="submit" type="submit" value="Login" tabindex="3"/>
